5e949120b202d8966ea09c57417d67f70b0ddb68
Franck Allimant <fallimant@openstudio.fr>
2021-09-17 12:25:06 +0200

    Payment checking is processed when returning on the shop

--- Controller/NotificationController.php
+++ Controller/NotificationController.php
@@ -23,12 +23,14 @@
 
 namespace Payline\Controller;
 
+use Payline\Exception\PaymentException;
 use Payline\Payline;
 use Payline\PaylineSDK;
 use Symfony\Component\Routing\Router;
 use Thelia\Core\Event\Order\OrderEvent;
 use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpFoundation\Response;
+use Thelia\Exception\TheliaProcessException;
 use Thelia\Log\Tlog;
 use Thelia\Model\Order;
 use Thelia\Model\OrderQuery;
@@ -43,25 +45,103 @@ use Thelia\Tools\URL;
  */
 class NotificationController extends BasePaymentModuleController
 {
+
+    public function payment3x(): void
+    {
+        $this->getSession()->set('isPayline3x', 1);
+    }
+
     /**
-     * @return Response|null
+     * Traiter la notification reçue du serveur PayLine
+     *
+     * @return Response
      * @throws \Exception
      */
-    public function notificationAction(): ?Response
+    public function notificationAction(): Response
     {
-        $resp = new Response();
+        try {
+            $this->processPaylineReturn('token');
 
-        if (null === $token = $this->getRequest()->get('token')) {
-            Tlog::getInstance()->error("Notification Payline appelée sans token. Réponse:".print_r($this->getRequest(), 1));
-            $resp->setContent("No token");
-            return $resp;
+            return new Response('OK');
+        } catch (\Exception $ex) {
+            // Echec: redirection vers la order-failed
+            return new Response($ex->getMessage());
+        }
+    }
+
+    /**
+     * Au retour du formulaire de paiement, analyser la réponse de payline
+     * et rediriger vers la page d'erreur ou de succes, selon le résultat.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function analyzePaymentReturnAction(): \Symfony\Component\HttpFoundation\Response
+    {
+        $frontOfficeRouter = $this->getContainer()->get('router.front');
+
+        try {
+            $order = $this->processPaylineReturn('paylinetoken');
+
+            // Succès: redirection vers la page order-placed
+            return $this->generateRedirect(
+                URL::getInstance()->absoluteUrl(
+                    $frontOfficeRouter->generate(
+                        "order.placed",
+                        ["order_id" => $order->getId()],
+                        Router::ABSOLUTE_URL
+                    )
+                )
+            );
+        } catch (PaymentException $ex) {
+            // Echec du paiement: redirection vers order-failed
+            return $this->generateRedirect(
+                URL::getInstance()->absoluteUrl(
+                    $frontOfficeRouter->generate(
+                        "order.failed",
+                        [
+                            "order_id" => $ex->getOrder()->getId(),
+                            "message" => $ex->getMessage()
+                        ],
+                        Router::ABSOLUTE_URL
+                    )
+                )
+            );
+        } catch (\Exception $ex) {
+            // Ici on n'a pas pu retrouver la commande : pas possible donc d'afficher order-failed
+            // on redirige vers la page d'erreur générique.
+            return $this->generateRedirect(URL::getInstance()->absoluteUrl('error'));
+        }
+    }
+
+    /**
+     * @param $tokenName
+     * @return Order
+     * @throws PaymentException|\Propel\Runtime\Exception\PropelException
+     */
+    protected function processPaylineReturn($tokenName): Order
+    {
+        $response = $this->getWebPaymentDetails($tokenName);
+
+        $order = $this->getOrderFromResponse($response);
+
+        // Si la commande est déjà payée (cas ou la notif de Payline arrive avant le retour sur le site),
+        // on ne fait rien, le traitement du paiement a déjà été effectué.
+        if (! $order->isPaid()) {
+            $this->checkPaymentResult($response, $order);
         }
 
-        $response = $this->getWebPaymentDetails($token);
+        return $order;
+    }
 
-        if (null !== $resp = $this->getOrderFromResponse($response, $order)) {
-            return $resp;
-        }
+    /**
+     * @throws PaymentException
+     * @throws \Exception
+     */
+    protected function checkPaymentResult(array $response, Order $order): void
+    {
+        $message = $this->getTranslator()->trans(
+            "Erreur technique: ID de transaction absent"
+        );
 
         if (isset($response['transaction']['id'])) {
             if ($response['result']['code'] === '00000') {
@@ -74,14 +154,21 @@ class NotificationController extends BasePaymentModuleController
 
                 $this->confirmPayment($order->getId());
 
-                $resp->setContent("OK");
-                return $resp;
+                return;
             }
 
             Tlog::getInstance()->info("Echec du paiement de la commande ".$order->getRef().", raison: ".$response['result']['code']);
+
+            $message = $this->getTranslator()->trans(
+                "Votre paiement a été refusé (code %code - %message)",
+                [
+                    '%code' => $response['result']['code'],
+                    '%message' => $response['result']['longMessage'] ?? 'raison inconnue'
+                ]
+            );
         }
 
-        Tlog::getInstance()->info("Echec du paiement de la commande ".$order->getRef());
+        Tlog::getInstance()->info("Echec du paiement de la commande ".$order->getRef(). ": $message");
 
         // Cancel the order
         $event = (new OrderEvent($order))
@@ -89,109 +176,52 @@ class NotificationController extends BasePaymentModuleController
 
         $this->dispatch(TheliaEvents::ORDER_UPDATE_STATUS, $event);
 
-        $resp->setContent("Order canceled");
-
-        return $resp;
-    }
-
-    public function payment3x(): void
-    {
-        $this->getSession()->set('isPayline3x', 1);
-    }
-
-
-    /**
-     * Analyser la réponse de payline et rediriger vers la page d'erreur ou de succes, selon le résultat.
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function analyzePaymentReturnAction()
-    {
-        $resp = new Response();
-
-        if (null === $token = $this->getRequest()->get('paylinetoken')) {
-            Tlog::getInstance()->error("Notification Payline appelée sans token. Réponse:".print_r($this->getRequest(), 1));
-            $resp->setContent("No token");
-            return $resp;
-        }
-
-        $response = $this->getWebPaymentDetails($token);
-
-        /** @var Order $order */
-        if (null !== $resp = $this->getOrderFromResponse($response, $order)) {
-            return $resp;
-        }
-
-        $frontOfficeRouter = $this->getContainer()->get('router.front');
-
-        // Succès: redirection vers la page order-placed
-        if ($response['result']['code'] === '00000') {
-            return $this->generateRedirect(
-                URL::getInstance()->absoluteUrl(
-                    $frontOfficeRouter->generate(
-                        "order.placed",
-                        ["order_id" => $order->getId()],
-                        Router::ABSOLUTE_URL
-                    )
-                )
-            );
-        }
-
-        // Echec: redirection vers la order-failed
-        return $this->generateRedirect(
-            URL::getInstance()->absoluteUrl(
-                $frontOfficeRouter->generate(
-                    "order.failed",
-                    [
-                        "order_id" => $order->getId(),
-                        "message" => $this->getTranslator()->trans(
-                            "Votre paiement a été refusé (code %code - %message)",
-                            [
-                                '%code' => $response['result']['code'],
-                                '%message' => $response['result']['longMessage'] ?? 'raison inconnue'
-                            ]
-                        )
-                    ],
-                    Router::ABSOLUTE_URL
-                )
-            )
-        );
+        throw new PaymentException($order, $message);
     }
 
     /**
      * @param array $response
-     * @param Order|null $order
-     * @return Response|null
+     * @return Order
+     * @throws TheliaProcessException
      */
-    protected function getOrderFromResponse(array $response, ?Order &$order): ?Response
+    protected function getOrderFromResponse(array $response): Order
     {
-        $resp = new Response();
-
         $privateData = $response['privateDataList']['privateData'] ?? [];
         $key = $privateData['key'] ?? null;
         $orderId = $privateData['value'] ?? null;
 
         if ($key !== 'orderId') {
             Tlog::getInstance()->error("Order ID absent de la réponse Payline.");
-            $resp->setContent("No order reference");
-            return $resp;
+            throw new TheliaProcessException(
+                $this->getTranslator()->trans("Echec du paiement: la référence de commande est absente", [], Payline::DOMAIN_NAME)
+            );
         }
 
         if (null === $order = OrderQuery::create()->findPk($orderId)) {
             Tlog::getInstance()->error("Pas de commande trouvée pour l'ID: ". $orderId);
-            $resp->setContent("Order not found");
-            return $resp;
+            throw new TheliaProcessException(
+                $this->getTranslator()->trans("Echec du paiement: la commande n'a pas pu être retrouvée", [], Payline::DOMAIN_NAME)
+            );
         }
 
-        return null;
+        return $order;
     }
 
     /**
-     * @param string $token
+     * @param string $tokenName
      * @return array
+     * @throws TheliaProcessException
      */
-    protected function getWebPaymentDetails(string $token): array
+    protected function getWebPaymentDetails(string $tokenName): array
     {
+        if (null === $token = $this->getRequest()->get($tokenName)) {
+            Tlog::getInstance()->error("Notification Payline appelée sans token. Réponse:".print_r($this->getRequest(), 1));
+
+            throw new TheliaProcessException(
+                $this->getTranslator()->trans("Erreur technique: le token PayLien est absent.", [], Payline::DOMAIN_NAME)
+            );
+        }
+
         $paylineSDK = new PaylineSDK(
             Payline::getConfigValue(Payline::MERCHANT_ID),
             Payline::getConfigValue(Payline::ACCESS_KEY),
@@ -212,6 +242,6 @@ class NotificationController extends BasePaymentModuleController
 
     protected function getModuleCode(): string
     {
-        return "Paylib";
+        return 'Paylib';
     }
 }
